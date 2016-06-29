725c6e46334777cbd993f58c995e454c521c9f96
Héctor Salazar <h.salazar.olivares@accenture.com>
2016-06-29 10:11:17 -0500

    Inline templates

--- app.rb
+++ app.rb
@@ -3,5 +3,45 @@
 require 'sinatra'
 
 get '/' do
-  "Hello there"
+  "Hello there, cowboy"
 end
+
+get'/greet/:name' do
+  "Hello, #{params[:name]}"
+end
+
+get '/this/*/is/*' do
+  params[:splat].join(" ")
+end
+
+get '/time' do
+  "Time is #{Time.now.strftime('%I:%M:%S %p')}"
+end
+
+get '/template' do
+  @title = "Fly me to the moon"
+  haml :form
+end
+
+post '/template' do
+  @title = "#{params[:post][:first_name]}"
+  haml "Hello, #{params[:post][:first_name]} #{params[:post][:last_name]}"
+end
+
+__END__
+
+@@layout
+%html
+  %head
+    %title= @title
+  =yield
+
+@@fly
+.title
+  %h1= @title
+
+@@form
+%form{action: "/template", method: "POST"}
+  %input{type: "text", name: "post[first_name]", size: 20}
+  %input{type: "text", name: "post[last_name]", size: 20}
+  %input{type: "submit", value: "Say hi"}
